024bab27b36f28f64a2c2343a896d2b678f1e344
abhaydiwan <abhaydiwan@gmail.com>
2018-04-05 11:44:11 -0400

    Create main.tf

---
+++ qa/main.tf
@@ -0,0 +1,12 @@
+# Provider configuration
+provider "aws" {
+  region = "us-east-1"
+}
+# Resource configuration
+resource "aws_instance" "hello-instance" {
+  ami = "ami-1853ac65"
+  instance_type = "t2.micro"
+  tags {
+    Name = "QA - Instance"
+  }
+}
